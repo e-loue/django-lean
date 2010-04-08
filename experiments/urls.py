9e7b226c7c1d571c6f3d4274caa363297a8d0137
rory@titanomachia <none@none>
2010-04-08 13:21:35 +0100

    include the admin stuff in the experiments url prefix (not sure this is the best way to do it, and it's a bit confusing having /experiments/admin *and* /admin/experiments doing 2 different things

--- experiments/urls.py
+++ experiments/urls.py
@@ -5,3 +5,7 @@ urlpatterns = patterns('experiments.views',
     url(r'^goal/(?P<goal_name>.*)$', 'record_experiment_goal'),
     url(r'^confirm_human/$', 'confirm_human')
 )
+
+urlpatterns += patterns('',
+    url(r'^admin/', include('experiments.admin_urls')),
+)
