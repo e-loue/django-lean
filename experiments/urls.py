66d87f741fde89c622647e53926474a8c37a38b8
rory@titanomachia <none@none>
2010-04-08 16:54:36 +0100

    undoing this change

--- experiments/urls.py
+++ experiments/urls.py
@@ -5,7 +5,3 @@ urlpatterns = patterns('experiments.views',
     url(r'^goal/(?P<goal_name>.*)$', 'record_experiment_goal'),
     url(r'^confirm_human/$', 'confirm_human')
 )
-
-urlpatterns += patterns('',
-    url(r'^admin/', include('experiments.admin_urls')),
-)
