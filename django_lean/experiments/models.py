a36214a803b95e28f572dfd5ec8fb77b48798f22
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-06-24 16:36:54 +0700

    Fixed to use settings.AUTH_USER_MODEL instead of directly importing User model from auth django.contrib.auth to confirm with Django 1.5+

--- django_lean/experiments/models.py
+++ django_lean/experiments/models.py
@@ -6,7 +6,6 @@ from datetime import date
 import random
 
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -271,7 +270,7 @@ class Participant(models.Model):
         (CONTROL_GROUP, "Control"),
         (TEST_GROUP, "Test"))
 
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
     experiment = models.ForeignKey(Experiment)
     enrollment_date = models.DateField(db_index=True, auto_now_add=True)
     group = models.IntegerField(choices=GROUPS)
